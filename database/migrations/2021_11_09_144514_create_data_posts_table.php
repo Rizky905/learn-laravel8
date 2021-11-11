d71199d92fd429a0e040e5487f9b2a68e4454ad8
RizkyErlangga <erlangga905@gmail.com>
2021-11-11 21:03:15 +0700

    migrate data pos

--- database/migrations/2021_11_09_144514_create_data_posts_table.php
+++ database/migrations/2021_11_09_144514_create_data_posts_table.php
@@ -14,6 +14,8 @@ class CreateDataPostsTable extends Migration
     public function up()
     {
         Schema::create('data_posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
             $table->string('title')->default('');
             $table->text('content')->default('');
         });
